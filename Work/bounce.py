2213022a511c82abaefc9aa7ea5ca9244fd2ee92
엄강우 <yhnb33@gmail.com>
2020-10-21 21:28:45 +0900

    1021 1. 파이썬 소개

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,7 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+for _ in range(10):
+    height *= 0.7
+    print(round(height, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,17 @@
 # mortgage.py
 #
 # Exercise 1.7
+month = 1
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+total_payment = 0.0
+
+while principal > 0:
+    total_payment = payment + 1000 if  61 <= month <= 108 else payment
+    month += 1
+    principal = principal * (1+rate/12) - total_payment
+    total_paid = total_paid + total_payment if principal > 0 else total_paid + total_payment + principal
+print(principal, total_payment)
+print('Total paid', total_paid)
